e28ed7d6f254bad9d0bb8c4e9c03d002216f4807
Rajat Khare <rdkhare123@gmail.com>
2021-06-07 19:01:24 -0700

    JSON commit

--- app.js
+++ app.js
@@ -4,18 +4,36 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 const exphbs = require('express-handlebars');
 var https = require('https')
-var fs = require('fs')
+var fs = require('fs');
+const resultJSON = require('./excel-convert');
 
 // Handlebars Middleware
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+app.use(logger);
+
 // Homepage Route
 
 app.get('/', (req, res) => res.render('index', {
     title: 'LiverTox App'
 }));
+
+// API Requests
+
+app.get('/api/hds-data', function(req, res) {
+    res.json(resultJSON["HDS"]);
+});
+app.get('/api/cases-data', (req, res) => res.json(resultJSON["Cases"]));
+app.get('/api/abstracts-data', (req, res) => res.json(resultJSON["Abstract"]));
+
+
 
 // https setup
 
